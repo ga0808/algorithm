d88253e9fa361fbc6a19567d336ef2b766e51db3
ga0808 <90363244+ga0808@users.noreply.github.com>
2024-01-24 21:50:17 +0900

    [level 2] Title: 올바른 괄호, Time: 7.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,12 @@
 def solution(s):
+    answer = True
     stack = []
     for i in s:
         if i == '(':
             stack.append(i)
-        else:
-            if stack == []: # 오른쪽 괄호로 시작할경우
-                return False
+        else: # i == ')'인 경우
+            if stack == []: 
+            return False
             else:
                 stack.pop()
-    if stack != []: # 다 끝났는데 왼쪽 괄호가 스택에 남아있을경우
-        return False
-    return True
+    return stack == []
