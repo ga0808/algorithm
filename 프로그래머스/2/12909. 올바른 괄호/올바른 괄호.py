9672605fd10c39c5b52180dd8d4319b919788619
ga0808 <90363244+ga0808@users.noreply.github.com>
2024-01-25 15:09:41 +0900

    [level 2] Title: 올바른 괄호, Time: 7.77 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,20 @@
+#스택으로 구현해보자
+
+#( ) - 쌍이 잘 맞아서 모두 없어졌을때 -> return T
+#) , ( - 남아버렸다 -> return False
+
 def solution(s):
-    answer = True
     stack = []
+    #( 여는 괄호를 stack에 넣은다음에, )이 나올때마다 삭제해줌 
     for i in s:
-        if i == '(':
+        if i == "(":
             stack.append(i)
-        else: # i == ')'인 경우
-            if stack == []: 
-            return False
+        else:
+            if stack ==[]: 
+                return False
             else:
                 stack.pop()
-    return stack == []
+    if stack != []: #for문 다 돌았는데, 여전히 여는 괄호가 남아있다면
+        return False
+    
+    return True
