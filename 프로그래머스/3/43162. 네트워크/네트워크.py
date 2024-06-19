e0680b75bbe3445f5105a14939f112ebfbe33d8c
ga0808 <90363244+ga0808@users.noreply.github.com>
2024-06-19 22:45:06 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,37 +2,23 @@ def solution(n, computers):
     answer = 0
     visited = [0]*n
     
+    #dfs : 연결된 컴퓨터들을 깊이 탐색 -> 연결된 하나의 네트워크를 탐색
     def dfs(row):
-        # nonlocal answer
-        visited[row] +=1
-        # print("visited",visited)
-        # print("row", row,"="*10)
-        #row : 0 > 1
+        if visited[row] == 1:
+                return 0
         
-#         if sum(computers[row]) == 1:
-#             answer += 1
-#             return 0
-
-#         if row == n-1 : 
-#             answer += 1 
-#             return 0
+        visited[row] += 1
         
-        
-        # print("computers[row]",computers[row])
         for idx, c in enumerate(computers[row]):
-            
-            if visited[idx] == 1 or idx == row:
-                # print("pass",idx, c)
+
+            if c == 0 or idx==row:
                 continue
+            dfs(idx)
             
-            # print("search",idx, c)
-            
-            if c == 1:
-                dfs(idx)  
-    
     for row, visit in enumerate(visited):
-        if visit == 0:
-            answer += 1
-            dfs(row)
+        if visit == 1:
+            continue
+        answer += 1
+        dfs(row)
     
     return answer
