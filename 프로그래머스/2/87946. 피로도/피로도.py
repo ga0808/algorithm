b93a3181d4bfe4343a28c2cedf9279ced4849856
ga0808 <90363244+ga0808@users.noreply.github.com>
2024-07-09 19:28:48 +0900

    [level 2] Title: 피로도, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,31 +1,29 @@
 def solution(k, dungeons):
-#모든 순열
-    max_explo_cnt = 0
+    max_cnt = 0
     
-    def dfs(before_idx,k,explo_cnt,visited):
-        nonlocal max_explo_cnt
+    def dfs(idx,k,cnt,visited):
+        nonlocal max_cnt
         
-        if explo_cnt > max_explo_cnt:
-            max_explo_cnt = explo_cnt
-            
-        if k==0:
+        if max_cnt < cnt:
+            max_cnt = cnt
+        
+        if k == 0:
             return 0
         
-        for idx, val in enumerate(dungeons):
-            if (idx == before_idx )or (k < val[0]) or (visited[idx]==1) : #지금 탐험한 것
+        for i, d in enumerate(dungeons):
+            if (d[0] > k) or (visited[i]==1):
                 continue
-            #탐험 가능한 것만 
-            copy_visited = visited[:]
-            copy_visited[idx] = 1
-            dfs(idx,k-val[1],explo_cnt+1,copy_visited)
             
-                
-    #처음부터 k보다 커서 탐험할 수 없는 던전 제거
-    for idx, d in enumerate(dungeons):
+            copy_visited = visited[:]
+            copy_visited[i] = 1
+            # print(d,cnt,copy_visited)
+            dfs(i,k-d[1],cnt+1,copy_visited)
+        
+    for idx,d in enumerate(dungeons):
         if d[0] > k:
             dungeons.pop(idx)
     
     visited = [0]*len(dungeons)
-    dfs(-1,k,0,visited)
+    dfs(0,k,0,visited)
     
-    return max_explo_cnt
+    return max_cnt
